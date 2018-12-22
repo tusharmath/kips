3b962b17d1f48e9564743556807b1e43bba02d11
Tushar Mathur <tusharmath@gmail.com>
2018-12-22 18:44:15 +0530

    feat(dir): add support to handle retries

--- generators/app/makeDirectories.ts
+++ generators/app/makeDirectories.ts
@@ -6,7 +6,31 @@ import * as fs from 'fs-extra'
 
 import * as Generator from 'yeoman-generator'
 
-export const createDirectories = async (gen: Generator) => {
-  await fs.mkdir(gen.destinationPath('src'))
-  await fs.mkdir(gen.destinationPath('test'))
+const createDirectoryMaker = (G: Generator) => async (path: string) => {
+  const gPath = G.destinationPath(path)
+  await fs.mkdir(gPath).catch(async <T extends {code: string}>(err: T) => {
+    if (err.code === 'EEXIST') {
+      const response = await G.prompt([
+        {
+          type: 'confirm',
+
+          default: false,
+          message: `Path "${path}" already exists, delete and retry`,
+          name: 'confirm'
+        }
+      ])
+      if (response.confirm) {
+        await fs.rmdir(path)
+        await createDirectoryMaker(G)(path)
+      }
+    } else {
+      throw err
+    }
+  })
+}
+
+export const createDirectories = async (G: Generator) => {
+  const mkDir = createDirectoryMaker(G)
+  await mkDir('src')
+  await mkDir('test')
 }
