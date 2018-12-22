9d6431f539c55f4148c871421628c4f17251aeb7
Tushar Mathur <tusharmath@gmail.com>
2018-12-22 18:44:35 +0530

    feat(prompt): auto store answers

--- generators/app/promptQuestions.ts
+++ generators/app/promptQuestions.ts
@@ -14,16 +14,19 @@ export const promptQuestions = async (gen: Generator) =>
       default: gen.appname.split(' ').join('-'),
       message: 'Project name',
       name: 'projectName',
+      store: true,
       type: 'input'
     },
     {
       message: 'Describe the project',
       name: 'projectDescription',
+      store: true,
       type: 'input'
     },
     {
       message: 'Keywords',
       name: 'keywords',
+      store: true,
       type: 'input'
     }
   ]) as Promise<IProjectProperties>
